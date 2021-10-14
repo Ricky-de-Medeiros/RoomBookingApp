59c53808a1e2c9c8a1befb72d68fa2324a05c062
Rick <rickmedeiros@gmail.com>
2021-10-15 10:35:41 +1300

    Add project files.

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,42 @@
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace RoomBookingApp.Core
+{
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Booking_Response_With_Request_Values()
+        {
+            // Arrange
+
+            var request = new RoomBookingRequest()
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2021, 10, 14)
+
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            // Act
+
+            RoomBookingResult result = processor.BookRoom(request);
+
+            // Assert
+            Assert.NotNull(result);
+            result.ShouldNotBeNull();
+
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+
+
+        }
+    }
+}
---
+++ RoomBookingApp.Core/Models/RoomBookingRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RoomBookingApp.Core.Models
+{
+    public class RoomBookingRequest
+    {
+        
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
